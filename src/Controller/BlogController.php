abb864f7c8464e97df9e79a3411e082a4553e144
William del Rosario <williamdelrosario@yahoo.com>
2019-03-07 07:43:56 +0100

    AbstractController request and response

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -10,6 +10,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -37,16 +38,20 @@ class BlogController extends AbstractController
     ];
 
     /**
-     * @Route("/", name="blog_list")
+     * @Route("/{page}", name="blog_list")
+     * @param Request $request
      * @param int $page
      *
      * @return JsonResponse
      */
-    public function list($page = 1)
+    public function list(Request $request, $page = 1)
     {
-        return new JsonResponse(
+        $limit = $request->get('limit', 10);
+
+        return $this->json(
             [
                 'page' => $page,
+                'limit' => $limit,
                 'data' => array_map(function ($item) {
                     return $this->generateUrl('blog_by_id', ['id' => $item['id']]);
                 }, self::POSTS)
@@ -62,7 +67,7 @@ class BlogController extends AbstractController
      */
     public function post($id)
     {
-        return new JsonResponse(
+        return $this->json(
             self::POSTS[array_search($id, array_column(self::POSTS, 'id'))]
         );
     }
@@ -75,7 +80,7 @@ class BlogController extends AbstractController
      */
     public function postBySlug($slug)
     {
-        return new JsonResponse(
+        return $this->json(
             self::POSTS[array_search($slug, array_column(self::POSTS, 'slug'))]
         );
     }
