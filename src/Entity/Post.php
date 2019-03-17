8370b4307dc60857efd67a7e6f96a1de867dca6b
William del Rosario <williamdelrosario@yahoo.com>
2019-03-17 18:22:44 +0100

    date filter

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -13,6 +13,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\DateFilter;
 
 /**
  * @ApiFilter(
@@ -23,6 +24,12 @@ use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
  *          "content"= "partial",
  *          "author"= "exact",
  *     }
+ * )
+ * @ApiFilter(
+ *     DateFilter::class,
+ *     properties={
+ *          "published"
+ *      }
  * )
  * @ApiResource(
  *     attributes={"order"={"published": "DESC"}},
