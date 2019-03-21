853c8cb92a63af369a0d4caa7d0b51b32e8df2e2
William del Rosario <williamdelrosario@yahoo.com>
2019-03-21 14:30:00 +0100

    added redux form reducer

--- public/react/src/reducer.js
+++ public/react/src/reducer.js
@@ -1,10 +1,12 @@
 import postList from "./reducers/postList";
-import {combineReducers} from "redux";
 import post from "./reducers/post";
 import commentList from "./reducers/commentList";
+import {combineReducers} from "redux";
+import {reducer as formReducer} from "redux-form";
 
 export default combineReducers({
     postList,
     post,
-    commentList
+    commentList,
+    form: formReducer
 });
