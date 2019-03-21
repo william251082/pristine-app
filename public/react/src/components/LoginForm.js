08d44297253a34df4b7ee1a19f4ce97a9a7e2da6
William del Rosario <williamdelrosario@yahoo.com>
2019-03-21 13:57:26 +0100

    redux-form

--- public/react/src/components/LoginForm.js
+++ public/react/src/components/LoginForm.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import {reduxForm} from "redux-form";
 
 class LoginForm extends React.Component
 {
     render() {
+        console.log(this.props);
         return(<div>Hello From Login Form!</div>)
     }
 }
 
-export default LoginForm;
+export default reduxForm({
+        form: 'LoginForm'
+    })(LoginForm);
