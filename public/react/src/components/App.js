bd7d5f6344bbf9da491d5817ffef133c32c42345
William del Rosario <williamdelrosario@yahoo.com>
2019-03-21 08:07:51 +0100

    header component - navigation

--- public/react/src/components/App.js
+++ public/react/src/components/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import LoginForm from "./LoginForm";
 import {Route, Switch} from "react-router";
 import PostListContainer from "./PostListContainer";
+import Header from "./Header";
 
 class App extends React.Component
 {
     render() {
         return(
             <div>
+                <Header/>
                 <Switch>
                     <Route path="/login" component={LoginForm}/>
-                    <Route path="/posts" component={PostListContainer}/>
+                    <Route path="/" component={PostListContainer}/>
                 </Switch>
             </div>
         )
