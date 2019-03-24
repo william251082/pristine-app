b6ad0d34f4c816d9ba548d25038283b1356af98c
William del Rosario <williamdelrosario@yahoo.com>
2019-03-24 20:54:10 +0100

    animations - animated comment adding

--- public/react/src/components/CommentList.js
+++ public/react/src/components/CommentList.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Message} from "./Message";
 import timeago from "timeago.js";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
+
+import "./CommentList.css";
 
 class CommentList extends React.Component
 {
@@ -13,21 +16,25 @@ class CommentList extends React.Component
 
         return (
             <div className="card mb-3 mt-3 shadow-sm">
-                { commentList.map(comment => {
-                    return (
-                        <div className="card-body border-bottom" key={comment.id}>
-                            <p className="card-text mb-0">
-                                { comment.content}
-                            </p>
-                            <p className="card-text">
-                                <small className="text-muted">
-                                    {timeago().format(comment.published)} by&nbsp;
-                                    {comment.author.name}
-                                </small>
-                            </p>
-                        </div>
-                    );
-                })}
+                <TransitionGroup>
+                    { commentList.map(comment => {
+                        return (
+                            <CSSTransition key={comment.id} timeout={1000} classNames="fade">
+                                <div className="card-body border-bottom">
+                                    <p className="card-text mb-0">
+                                        { comment.content}
+                                    </p>
+                                    <p className="card-text">
+                                        <small className="text-muted">
+                                            {timeago().format(comment.published)} by&nbsp;
+                                            {comment.author.name}
+                                        </small>
+                                    </p>
+                                </div>
+                            </CSSTransition>
+                        );
+                    })}
+                </TransitionGroup>
             </div>
         )
     }
