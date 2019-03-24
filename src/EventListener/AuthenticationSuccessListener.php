ad6b128b4d5266dfe6af3c2102a7005f7e7314e1
William del Rosario <williamdelrosario@yahoo.com>
2019-03-24 07:41:01 +0100

    userId on api/login_check endpoint

---
+++ src/EventListener/AuthenticationSuccessListener.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: williamdelrosario
+ * Date: 2019-03-24
+ * Time: 07:30
+ */
+
+namespace App\EventListener;
+
+use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+
+class AuthenticationSuccessListener
+{
+    public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
+    {
+        $data = $event->getData();
+
+        $user = $event->getUser();
+
+        if (!$user instanceof User) {
+            return;
+        }
+
+        $data['id'] = $user->getId();
+
+        $event->setData($data);
+    }
+}
