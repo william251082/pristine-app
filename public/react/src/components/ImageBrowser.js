e2c6ee65a95f495d29de9ec0ab9884fab40606bb
William del Rosario <williamdelrosario@yahoo.com>
2019-03-25 19:57:45 +0100

    image browser animations

--- public/react/src/components/ImageBrowser.js
+++ public/react/src/components/ImageBrowser.js
@@ -1,24 +1,32 @@
 import React from "react";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 
-export class ImageBrowser extends React.Component
-{
+export class ImageBrowser extends React.Component {
     render() {
         const {images} = this.props;
 
         return (
             <div className="row mt-4 mb-4">
-                {
-                    images.map(image => {
-                        return (
-                            <div key={image.id}  className="col-md-6 col-lg-4">
-                                <div className="mt-2 mb-2">
-                                    <img src={`http://localhost:8000${image.url}`}
-                                         className="img-fluid"/>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
+                <TransitionGroup component={null}>
+                    {
+                        images.map(image => {
+                            return (
+                                <CSSTransition timeout={1000} classNames="fade" key={image.id}>
+                                    <div className="col-md-6 col-lg-4">
+                                        <div className="mt-2 mb-2">
+                                            <img src={`http://localhost:8000${image.url}`}
+                                                 className="img-fluid"/>
+                                        </div>
+                                        <div className="mb-2">
+                                            <button type="button"
+                                                    className="btn btn-outline-danger btn-sm">Remove</button>
+                                        </div>
+                                    </div>
+                                </CSSTransition>
+                            )
+                        })
+                    }
+                </TransitionGroup>
             </div>
         )
     }
