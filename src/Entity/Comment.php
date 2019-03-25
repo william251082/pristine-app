7c0f6b8a1a6690f3d485977865c6f499a6cad2b6
William del Rosario <williamdelrosario@yahoo.com>
2019-03-25 09:10:04 +0100

    modified api - missing user embedded data on new comment

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -25,7 +25,10 @@ use Symfony\Component\Validator\Constraints as Assert;
  *     collectionOperations={
  *          "get",
  *          "post"={
- *              "access_control"="is_granted('ROLE_COMMENTATOR')"
+ *              "access_control"="is_granted('ROLE_COMMENTATOR')",
+ *              "normalization_context"={
+ *                  "groups"={"get-comment-with-author"}
+ *              }
  *          },
  *          "api_posts_comments_get_subresource"={
  *              "normalization_context"={
